c2fa47f4dd8e3be03994e9111ad4b236d5808e1a
Twiliness <729294652hs@gmail.com>
2025-10-24 01:11:27 +0000

    [Build]: fix build errors
    Signed-off-by: Twiliness <729294652hs@gmail.com>

--- lib/serde-yaml/src/libyaml/error.rs
+++ lib/serde-yaml/src/libyaml/error.rs
@@ -18,33 +18,27 @@ pub(crate) struct Error {
 impl Error {
     pub unsafe fn parse_error(parser: *const sys::yaml_parser_t) -> Self {
         Error {
-            kind: (*parser).error,
-            problem: match NonNull::new((*parser).problem as *mut _) {
+            kind: (&*parser).error,
+            problem: match NonNull::new((&*parser).problem as *mut _) {
                 Some(problem) => CStr::from_ptr(problem),
                 None => CStr::from_bytes_with_nul(b"libyaml parser failed but there is no error\0"),
             },
-            problem_offset: (*parser).problem_offset,
-            problem_mark: Mark {
-                sys: (*parser).problem_mark,
-            },
-            context: match NonNull::new((*parser).context as *mut _) {
+            problem_offset: (&*parser).problem_offset,
+            problem_mark: Mark { sys: (&*parser).problem_mark },
+            context: match NonNull::new((&*parser).context as *mut _) {
                 Some(context) => Some(CStr::from_ptr(context)),
                 None => None,
             },
-            context_mark: Mark {
-                sys: (*parser).context_mark,
-            },
+            context_mark: Mark { sys: (&*parser).context_mark },
         }
     }
 
     pub unsafe fn emit_error(emitter: *const sys::yaml_emitter_t) -> Self {
         Error {
-            kind: (*emitter).error,
-            problem: match NonNull::new((*emitter).problem as *mut _) {
+            kind: (&*emitter).error,
+            problem: match NonNull::new((&*emitter).problem as *mut _) {
                 Some(problem) => CStr::from_ptr(problem),
-                None => {
-                    CStr::from_bytes_with_nul(b"libyaml emitter failed but there is no error\0")
-                }
+                None => CStr::from_bytes_with_nul(b"libyaml emitter failed but there is no error\0"),
             },
             problem_offset: 0,
             problem_mark: Mark {
--- lib/serde-yaml/src/libyaml/parser.rs
+++ lib/serde-yaml/src/libyaml/parser.rs
@@ -84,17 +84,16 @@ impl<'input> Parser<'input> {
         let mut event = MaybeUninit::<sys::yaml_event_t>::uninit();
         unsafe {
             let parser = addr_of_mut!((*self.pin.ptr).sys);
-            if (*parser).error != sys::YAML_NO_ERROR {
+            if (&*parser).error != sys::YAML_NO_ERROR {
                 return Err(Error::parse_error(parser));
             }
             let event = event.as_mut_ptr();
             if sys::yaml_parser_parse(parser, event).fail {
                 return Err(Error::parse_error(parser));
             }
-            let ret = convert_event(&*event, &(*self.pin.ptr).input);
-            let mark = Mark {
-                sys: (*event).start_mark,
-            };
+            let pinned = &*self.pin.ptr;
+            let ret = convert_event(&*event, &pinned.input);
+            let mark = Mark { sys: (&*event).start_mark };
             sys::yaml_event_delete(event);
             Ok((ret, mark))
         }
